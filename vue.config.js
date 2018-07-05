3d5ca954a7ba563e65d79a6f5f44d94f42fb6463
Corey Coburn <corey@coburn.codes>
2018-07-05 15:15:02 -0700

    added tailwind and purgecss

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, "./src/*.vue"),
+          path.join(__dirname, "./src/components/**/*.vue"),
+          path.join(__dirname, "./src/views/**/*.vue")
+        ])
+      })
+    ]
+  }
+};
