e6a927e58d64de1cf67fd54b1146dea615ded5a9
Corey Coburn <corey@coburn.codes>
2018-07-06 02:39:53 -0700

    remove purgecss temporarily

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-const glob = require("glob-all");
+// const path = require('path')
+// const PurgecssPlugin = require('purgecss-webpack-plugin')
+// const glob = require('glob-all')
 
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, "./src/*.vue"),
-          path.join(__dirname, "./src/components/**/*.vue"),
-          path.join(__dirname, "./src/layouts/**/*.vue"),
-          path.join(__dirname, "./src/pages/**/*.vue")
-        ])
-      })
-    ]
-  }
-};
+// module.exports = {
+//     configureWebpack: {
+//         plugins: [
+//             new PurgecssPlugin({
+//                 paths: glob.sync([
+//                     path.join(__dirname, './src/*.vue'),
+//                     path.join(__dirname, './src/components/**/*.vue'),
+//                     path.join(__dirname, './src/layouts/**/*.vue'),
+//                     path.join(__dirname, './src/pages/**/*.vue')
+//                 ])
+//             })
+//         ]
+//     }
+// }
